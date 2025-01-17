9e39bff0ca601307a180656357b3ebe1a815d051
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2025-01-17 15:21:35 -0500

    Mejorar la estructura y estilos del componente NavbarItem, ajustando la disposición y la visibilidad del texto de etiqueta.

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
-const NavbarItem = ({Icon, href, label}) => {
+const NavbarItem = ({ Icon, href, label }) => {
   return (
-    <div className="relative hover:scale-110 group transition-all">
-          <a href={href}>
-            <Icon className="group-hover:text-white" size={22} />
-          </a>
-          <p className="bg-black/30 py-1 px-3 opacity-0 left-1/2 -translate-x-1/2 top-10 text-sm rounded cursor-default invisible group-hover:visible group-hover:opacity-100 absolute transition-all">
-            {label}
-          </p>
-        </div>
-  )
-}
+    <a href={href}>
+      <div className="relative hover:scale-110 hover:-translate-y-1 group transition-all">
+        <Icon className="group-hover:text-white" size={22} />
+        <p className="bg-black/30 py-1 px-3 opacity-0 left-1/2 -translate-x-1/2 top-12 text-sm font-medium rounded cursor-default invisible group-hover:visible group-hover:opacity-100 absolute transition-all">
+          {label}
+        </p>
+      </div>
+    </a>
+  );
+};
 
-export default NavbarItem
+export default NavbarItem;
