08b39483f00f1ae10097636f18f6c0bae84e4228
juanmayorquin <juanjmayor@outlook.es>
2025-01-30 22:44:23 -0500

    Se añade target _blank en Anchor

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const Anchor = ({ Icon, href }) => {
   return (
-    <a href={href}>
+    <a target="_blank" href={href}>
       <div className="text-white bg-orange-600 p-3 rounded-xl hover:text-neutral-800 transition-all">
         <Icon size={24} />
       </div>
