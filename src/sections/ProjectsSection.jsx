736e56512dbe564e23719524937084b537076d4c
juanmayorquin <juanjmayor@outlook.es>
2025-01-30 22:29:21 -0500

    Actualizar la sección de proyectos con nuevos proyectos, imágenes, descripciones y enlaces de despliegue y GitHub.

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -4,28 +4,38 @@ import Section from "../components/Section";
 const ProjectsSection = () => {
   const projects = [
     {
-      img: "/projectsImg/screenshot.png",
-      title: "Project Title",
-      description: "Short and clear project description",
-      deployLink: "#",
-      gitHubLink: "#",
-      tags: ["HTML", "CSS", "JavaScript", "React", "MongoDB", "Redux"],
+      img: "/projectsImg/Vroom-co-ecommerce.webp",
+      title: "Vroom Co. E-Commerce",
+      description: "E-Commerce for a clothes brand",
+      deployLink: "https://vroom-co.vercel.app/",
+      gitHubLink: "https://github.com/juanmayorquin/vroom-co-ecommerce",
+      tags: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TailwindCSS",
+        "React",
+        "Firebase",
+        "Firestore",
+      ],
     },
     {
-      img: "/projectsImg/screenshot.png",
-      title: "Another Project Title",
-      description: "Another short and clear project description",
-      deployLink: "#",
-      gitHubLink: "#",
-      tags: ["HTML", "CSS", "TypeScript", "Preact"],
+      img: "/projectsImg/Memorias.webp",
+      title: "Memorias | 50 años entre historias",
+      description:
+        "Sistema multimedia interactivo basado en Memorias, celebrando los 50 años de la UAO",
+      deployLink: "https://juanmayorquin.github.io/memorias/",
+      gitHubLink: "https://github.com/juanmayorquin/memorias",
+      tags: ["HTML", "CSS", "JavaScript", "TailwindCSS", "React"],
     },
     {
-      img: "/projectsImg/screenshot.png",
-      title: "A Random Project ",
-      description: "Another short and clear project description",
-      deployLink: "#",
-      gitHubLink: "#",
-      tags: ["HTML", "CSS", "TypeScript"],
+      img: "/projectsImg/BioARLab.webp",
+      title: "BioARLab",
+      description:
+        "Sistema multimedia interactivo que permite vistas AR de elementos de laboratorio de la carrera Ing. BiomédicaF",
+      deployLink: "https://juanmayorquin.github.io/visor-ar/",
+      gitHubLink: "https://github.com/juanmayorquin/visor-ar",
+      tags: ["HTML", "CSS", "TailwindCSS", "React"],
     },
   ];
   return (
